681c55fa6addf013f6de16acb3dc712604bc88a1
Rad <118402014+DarrGilreath@users.noreply.github.com>
2023-04-06 22:37:33 -0400

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,12 +1,13 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
- apiKey: 'dff81fe46b82475da30328ec139c53d4' 
+ apiKey: '' 
+ // Add your own apiKey above
 });
 
 
 const handleApiCall = (req, res) => {
-  // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+  // Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
   // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
   // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
   // If that isn't working, then that means you will have to wait until their servers are back up. 
